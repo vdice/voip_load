f5fe273126bc45843268ce85bf2da062b841eb02
vdice <vaughn.dice@readytalk.com>
2015-01-13 13:38:16 -0700

    loadme.rb now has more param override options; slimming down Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
 FROM ruby:2.1.2
 
-RUN gem install selenium 
-RUN selenium install
-RUN gem install selenium-webdriver 
+RUN gem install selenium && selenium install && gem install selenium-webdriver 
 
 ADD ./scripts/ /home/root/scripts
-
-EXPOSE 4444 5999
 
 ENTRYPOINT ["/home/root/scripts/loadme.rb"]
--- scripts/loadme.rb
+++ scripts/loadme.rb
@@ -4,14 +4,13 @@ require 'selenium-webdriver'
 require 'net/http'
 require 'optparse'
 
-ACCESS_NUMBER = "8667401260"
-ACCESS_CODE = "2698043"
+DEFAULT_ACCESS_NUMBER = "8667401260"
+DEFAULT_ACCESS_CODE = "TESTING"
 DEFAULT_WEB_APP_HOST = "http://web:80"
 DEFAULT_INSTANCE_NUM = 1
-SELENIUM_HUB_URLS = ["http://client:4444/wd/hub"]
+SELENIUM_HUB_URLS = ["http://hub:4444/wd/hub"]
 CHROME_SWITCHES = %w[ --use-fake-device-for-media-stream --use-fake-ui-for-media-stream ]
 CALL_BUTTON_ENABLED_TIMEOUT = 10
-
 
 def is_integer(arg)
   return arg =~ /\A\d+\z/
@@ -25,24 +24,45 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage:  [options]"
 
+  opts.on('-t', '--talk', 'clients will send audio if set') do |v|
+    options[:talk] = v 
+  end
   opts.on('-n', '--number NUMBER', 'number of clients to load') do |v|
     options[:number] = v 
   end
-  opts.on('-w', '--webapp WEBAPP', 'web app url') do |v| 
+  opts.on('--accessNumber ACCESS_NUMBER', "default: #{DEFAULT_ACCESS_NUMBER}") do |v| 
+    options[:accessNumber] = v
+  end
+  opts.on('--accessCode ACCESS_CODE', "default: #{DEFAULT_ACCESS_CODE}") do |v| 
+    options[:accessCode] = v
+  end
+  opts.on('--webapp WEBAPP', "default: #{DEFAULT_WEB_APP_HOST}") do |v| 
     options[:webapp] = v
+  end
+  opts.on('--freeswitchServers FREESWITCH_SERVERS', 'comma-separated list') do |v| 
+    options[:freeswitchservers] = v
+  end
+  opts.on('--turnServers TURN_SERVERS', 'comma-separated list') do |v| 
+    options[:turnservers] = v
   end
 
 end.parse!
 
 instance_num = is_integer(options[:number]) ? Integer(options[:number]) : DEFAULT_INSTANCE_NUM
 web_app_host = is_present(options[:webapp]) ? options[:webapp] : DEFAULT_WEB_APP_HOST
+access_number = is_present(options[:accessNumber]) ? options[:accessNumber] : DEFAULT_ACCESS_NUMBER
+access_code = is_present(options[:accessCode]) ? options[:accessCode] : DEFAULT_ACCESS_CODE
+freeswitch_servers = is_present(options[:freeswitchservers]) ? options[:freeswitchservers] : nil
+turn_servers = is_present(options[:turnservers]) ? options[:turnservers] : nil
 
-puts 'Using instance number = %d' % instance_num
-puts 'Using web app host = %s' % web_app_host
+puts "Using instance number = #{instance_num}"
 
-web_app_url = web_app_host + "/#/?an=#{ACCESS_NUMBER}&ac=#{ACCESS_CODE}"
-web_app_url_talk = web_app_url + "&mode=testAudio"
+web_app_url = web_app_host + "/voip-client/#/?an=#{access_number}&ac=#{access_code}"
+web_app_url << "&freeswitchServers=#{freeswitch_servers}" if freeswitch_servers
+web_app_url << "&turnServers=#{turn_servers}" if turn_servers
+web_app_url << "&mode=testAudio" if options[:talk]
 
+puts "Using web app url = #{web_app_url}"
 
 $drivers = []
 
@@ -54,7 +74,7 @@ count = 0
 for hub in SELENIUM_HUB_URLS
   (1..instance_num).each do |n|
     driver = Selenium::WebDriver.for(:chrome, :url => hub, :switches => CHROME_SWITCHES)
-    driver.navigate.to web_app_url_talk
+    driver.navigate.to web_app_url
     
     callButton = driver.find_element(:id, 'callButton')
     count = count + 1
